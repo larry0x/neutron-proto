b581ead4025c9de42cf2d00f434f88f4a16c66fd
larry <26318510+larry0x@users.noreply.github.com>
2023-06-20 19:27:42 +0100

    update lib.rs

---
+++ neutron-proto/src/lib.rs
@@ -0,0 +1,29 @@
+pub const NEUTRON_VERSION: &str = include_str!("prost/neutron/NEUTRON_COMMIT");
+
+pub mod neutron {
+    pub mod contractmanager {
+        include!("prost/neutron/neutron.contractmanager.rs");
+    }
+
+    pub mod cron {
+        include!("prost/neutron/neutron.cron.rs");
+    }
+
+    pub mod feeburner {
+        include!("prost/neutron/neutron.feeburner.rs");
+    }
+
+    pub mod interchainqueries {
+        include!("prost/neutron/neutron.interchainqueries.rs");
+    }
+
+    pub mod interchaintxs {
+        pub mod v1 {
+            include!("prost/neutron/neutron.interchaintxs.v1.rs");
+        }
+    }
+
+    pub mod transfer {
+        include!("prost/neutron/neutron.transfer.rs");
+    }
+}
